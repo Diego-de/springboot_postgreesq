732639f4e66fe72f23159213059348d9f62b1a45
Diego-de <diego04dgs@gmail.com>
2022-04-21 19:10:33 -0300

    first commit

---
+++ src/test/java/com/example/atividadeSpring_PostgreeSql/AtividadeSpringPostgreeSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadeSpring_PostgreeSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeSpringPostgreeSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
